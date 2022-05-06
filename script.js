7cbb60bb47b1d5095191423220bea6234e6d0242
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-06 10:49:52 -0300

    Adding the constructor method and class

--- script.js
+++ script.js
@@ -3,27 +3,26 @@ let fields = document.querySelectorAll("#form-user-create [name]");
 // criando uma Variavel que armazena um JSON
 let user = {};
 
+// Funcao para inserir no HTML
 function addLine(dataUser) {
 
-    let tr = document.createElement("tr");
+    console.log(dataUser);
 
-    tr.innerHTML = ` 
+    // Inserindo conteudo no HTML Utilizando tamplteString
+    document.getElementById('table-users').innerHTML = `
+
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm" /></td>
-    <td>${dataUser.name}</td>
-    <td>${dataUser.email}</td>
-    <td>${dataUser.admin}</td>
-    <td>${dataUser.birth}</td>
-    <td>
-      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-    </td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
   </tr>
   `;
-
-    document.getElementById('table-users').appendChild(tr);
-
-    console.log('addline', dataUser)
 }
 
 // Evento Para receber os dados ao clicar no button submit
@@ -44,7 +43,19 @@ document.getElementById('form-user-create').addEventListener("submit", function 
     });
 
 
-    addLine(user)
+    let objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+
+    );
+
+    addLine(objectUser)
 
 });
 
