ce6828cfd6705b07abd0ec2bdbc2a231b9ee80f1
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-06 08:56:16 -0300

    User project

---
+++ script.js
@@ -0,0 +1,10 @@
+// Pegando objetos no HTML e atribuindo a variaveis
+let Name = document.querySelector('#exampleInputName');
+let gender = document.querySelector("#form-user-create [name=gender]:checked");
+let birth = document.querySelector("#exampleInputBirth");
+let country = document.querySelector('#exampleInputCountry');
+let email = document.querySelector('#exampleInputEmail');
+let password = document.querySelector('#exampleInputPassword');
+let photo = document.querySelector('#exampleInputFile')
+let admin = document.querySelector('#exampleInputAdmin')
+
