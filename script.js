e53ee8b754f59ca453231df98a1c089709233b10
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-06 09:35:45 -0300

    adding IF and JSON

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// Pegando objetos no HTML e atribuindo a variaveis
-let Name = document.querySelector('#exampleInputName');
-let gender = document.querySelector("#form-user-create [name=gender]:checked");
-let birth = document.querySelector("#exampleInputBirth");
-let country = document.querySelector('#exampleInputCountry');
-let email = document.querySelector('#exampleInputEmail');
-let password = document.querySelector('#exampleInputPassword');
-let photo = document.querySelector('#exampleInputFile')
-let admin = document.querySelector('#exampleInputAdmin')
+// pegando todos valores do formulario de uma vez
+let fields = document.querySelectorAll("#form-user-create [name]");
+let user = {};
 
+
+fields.forEach(function (field, index) {
+
+    if (field.name == "gender") {
+
+        if (field.checked) {
+            user[field.name] = field.value;
+        }
+
+    } else {
+        user[field.name] = field.value
+    }
+});
+
+console.log(user);
