7e75d018d223a452277e6d83b9e979538a948734
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-10 10:28:44 -0300

    Adding delete function in LocalStorage

--- controllers/userController.js
+++ controllers/userController.js
@@ -195,23 +195,9 @@ class UserController {
     }
 
 
-    getUsersStorage() {
-
-        let users = [];
-
-        if (localStorage.getItem("users")) {
-
-            users = JSON.parse(localStorage.getItem("users"));
-
-        }
-
-        return users;
-    }
-
-
     selectAll() {
 
-        let users = this.getUsersStorage();
+        let users = User.getUsersStorage();
 
         users.forEach(dataUser => {
 
@@ -266,7 +252,15 @@ class UserController {
 
             if (confirm("Deseja realmente excluir o Usuario ?")) {
 
+                let user = new User();
+
+                user.loadFromJSON(JSON.parse(tr.dataset.user));
+
+                user.remove();
+
+                // removo nativo do JS
                 tr.remove();
+
                 this.updateCount();
             }
 
